d9ad8e38569d5254c76b1e0c17f4228cb93971b4
Vinicius <draco-vinicius@hotmail.com>
2026-09-17 15:12:46 -0400

    Update ContactPage.jsx

--- src/assets/Presentational/ContactPage.jsx
+++ src/assets/Presentational/ContactPage.jsx
@@ -6,26 +6,26 @@ function ContactPage(props){
   <h2 id="Reference">./Contact Me</h2>
   <h2 id="Title">Contact Me</h2>
 
-  <img src="/Img_Sources/Email_logo.png" id="emailLogo"/>
+  <img src={`${import.meta.env.BASE_URL}Img_Sources/Email_logo.png`} id="emailLogo"/>
   <div className="info-card" id="emailId">
       <h2>Email</h2>
       <p>vinicius.soaresds@outlook.com</p>
   </div>
 
-  <img src="/Img_Sources/LinkedIn_logo.png" id="linkedinLogo"/>
+  <img src={`${import.meta.env.BASE_URL}Img_Sources/LinkedIn_logo.png`} id="linkedinLogo"/>
   <div className="info-card" id="linkedinId">
       
       <h2>LinkedIn</h2>
       <p><a href="github.com/Vinicius-Soares-Santana"target="_blank">linkedin.com/in/vinisoaressantana</a></p>
   </div>
 
-  <img src="/Img_Sources/Instagram icon.png" id="instagramLogo"/>
+  <img src={`${import.meta.env.BASE_URL}Img_Sources/Instagram icon.png`} id="instagramLogo"/>
   <div className="info-card" id="instagramId">
       <h2>Instagram</h2>
       <p>@vinisoaressantana</p>
   </div>
 
-  <img src="/Img_Sources/GithubLogo.svg.webp" id="githubLogo"/>
+  <img src={`${import.meta.env.BASE_URL}Img_Sources/GithubLogo.svg.webp`} id="githubLogo"/>
   <div className="info-card" id="githubId">
       <h2>Github</h2>
       <p><a href="github.com/Vinicius-Soares-Santana" target="_blank">github.com/Vinicius-Soares-Santana</a></p>
