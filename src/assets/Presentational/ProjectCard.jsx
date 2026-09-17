f42a188fede8ce37369c67b895308b777392cbef
Vinicius <draco-vinicius@hotmail.com>
2026-09-17 10:48:25 -0400

    Update ProjectCard.jsx

--- src/assets/Presentational/ProjectCard.jsx
+++ src/assets/Presentational/ProjectCard.jsx
@@ -8,7 +8,7 @@ function ProjectCard(props){
   const playOrNot = (playvar) => {
     if(playvar || playvar == "true"){
       return  <div className="project-card">
-            <img src={`${${import.meta.env.BASE_URL}${project.image}`} />
+            <img src={`${import.meta.env.BASE_URL}${project.image}`} />
             <h4 className="cardTitle" >{project.title}</h4>
             <p className="cardDescription">{project.description}</p>
             <button className="GitHub" onClick={()=>{window.open(project.github, "_blank")}}>GitHub</button>
@@ -16,7 +16,7 @@ function ProjectCard(props){
           </div>
     }else{
       return  <div className="project-card">
-            <img src={`${${import.meta.env.BASE_URL}${project.image}`} />
+            <img src={`${import.meta.env.BASE_URL}${project.image}`} />
             <h4 className="cardTitle" >{project.title}</h4>
             <p className="cardDescription">{project.description}</p>
             <button className="GitHub" onClick={()=>{window.open(project.github, "_blank")}}>GitHub</button>
